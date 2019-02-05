9a6ce736f24a0e867630a00164f6cc95dc7e2b96
Andrey Kuzubov <smoll9521@gmail.com>
2019-02-05 23:02:39 +0300

    coroutine daemon

---
+++ src/main/kotlin/coroutine/CoroutineDaemon.kt
@@ -0,0 +1,17 @@
+package coroutine
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = GlobalScope.launch { // launch new coroutine and keep a reference to its Job
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    while (job.isActive)
+        Thread.sleep(100)
+    job.join()
+}
